f4c878ee6e70ef3b54f5e3449a2cb863111db222
Jason Jackson <jasonjckn@gmail.com>
2025-03-07 19:45:27 -0800

    fixup! Fix quote issue for helper 2 installation

--- Formula/emacs-mac.rb
+++ Formula/emacs-mac.rb
@@ -10,7 +10,7 @@ class EmacsMac < Formula
   desc "YAMAMOTO Mitsuharu's Mac port of GNU Emacs"
   homepage "https://www.gnu.org/software/emacs/"
   stable do
-    url "https://bitbucket.org/mituharu/emacs-mac/get/65c6c96f27afa446df6f9d8eff63f9cc012cc738.tar.gz"
+    url "https://bitbucket.org/mituharu/emacs-mac2/get/65c6c96f27afa446df6f9d8eff63f9cc012cc738.tar.gz"
     version "emacs-29.1-mac-10.0"
     sha256 "54d7ba79157c8cb7c3e20be5ce0fbcddd3d5bd0b339b11bc628d7c67a4765b9b"
     patch do
@@ -23,7 +23,7 @@ class EmacsMac < Formula
   end
 
   head do
-    url "https://bitbucket.org/mituharu/emacs-mac.git", branch: "work"
+    url "https://github.com/jasonjckn/emacs-mac.git", branch: "work_30"
     patch do
       # patch for multi-tty support, see the following links for details
       # https://bitbucket.org/mituharu/emacs-mac/pull-requests/2/add-multi-tty-support-to-be-on-par-with/diff
